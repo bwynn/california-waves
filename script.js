64b4b7243b2ccbf3a5126933a96c22f8126305f2
Brian Wynn <brian.wynn83@gmail.com>
2014-08-18 13:21:52 -0700

    Adding pulled data
    Requests sent to WorldWeatherOnline api for live weather data.

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ $(document).ready(function() {
 	
 	// Appends a link onto the end of each 
 	// wave's unordered list description
-	var message = $("<a href='http://www.surfline.com'>Check current conditions</a>");
-	$("button", ".wavesContainer").before(message);
+	//var message = $("<a href='http://www.surfline.com'>Check current conditions</a>");
+	//$("button", ".wavesContainer").before(message);
 	
-	//Random wave of the day
-	//This randomly selects between the different 
-	//div containers with a .waves class	
+	// Random wave of the day
+	// This randomly selects between the different 
+	// div containers with a .waves class	
 	$("#predict").on("click", ".bestbet" , function() {
 		var waves = $(".wavesContainer");
 		var items = waves.find(".waves");
@@ -45,7 +45,7 @@ $(document).ready(function() {
                 .on("mouseleave", "li", showPhotos);
 				
 				
-	//Identifies the day of the week
+	// Identifies the day of the week
 	var d = new Date();
 	var weekday = new Array(7);
 	weekday[0] = "sunday";
@@ -57,10 +57,184 @@ $(document).ready(function() {
 	weekday[6] = "saturday";
 	var today = weekday[d.getDay()];
 	
-	//Changes main poster image every day
-	//Using above today variable and changing the string of the 
-	//image url to get us to each day's featured poster image
+	// Changes main poster image every day
+	// Using above today variable and changing the string of the 
+	// image url to get us to each day's featured poster image
 	$(".poster").css("background-image", "url(img/poster_" + today + ".jpg)");
 	$(".poster").addClass("mainImg");
+	
+	// Steamer Lane Weather conditions
+		var waves = $(this);
+		var api = "c9cda4e16df76d61eb092e6b5c5910ee3f0c6f3c";
+		var staCzUrl = "http://api.worldweatheronline.com/free/v1/weather.ashx?q=95062&format=json&date=today&key=" + api;
+		$.ajax({
+			type: 'POST',
+			url: staCzUrl,
+			dataType: 'jsonp',
+			data: waves.serialize(),
+			success: function(data){
+				
+				// local time 
+				console.log(data.data.current_condition[0].observation_time);
+				var gmt = data.data.current_condition[0].observation_time;
+				$('#steamers .gmt').text('Observation Time: ' + gmt + ' GMT');
 
+				//temperature
+				console.log(data.data.current_condition[0].temp_F);
+				var temp = data.data.current_condition[0].temp_F;
+				$('#steamers .temp').text('Air Temp: ' + temp + ' degrees.');
+				
+				//wind direction
+				console.log(data.data.current_condition[0].winddirDegree);
+				var w_dir = data.data.current_condition[0].winddirDegree;
+				
+				if (w_dir < 0) {
+				var windDir = "North";
+					} else if (w_dir < 45) {
+				var windDir = "North East";		
+						} else if (w_dir < 90) {
+				var windDir = "East";			
+						} else if (w_dir < 135) {
+				var windDir = "South East";	
+							} else if (w_dir < 180) {
+				var windDir = "South";				
+								} else if (w_dir < 225) {
+				var windDir = "South West";		
+									} else if (w_dir < 270) {
+				var windDir = "West";	
+										} else if (w_dir < 315) {
+				var windDir = "North West";							
+											}
+				console.log(windDir);
+				
+				// wind speed
+				console.log(data.data.current_condition[0].windspeedMiles);
+				var w_speed = data.data.current_condition[0].windspeedMiles;
+				$('#steamers .windDir').text('Wind: From the ' + windDir + ' at ' + w_speed + ' mph');
+				
+				// weather Description
+				console.log(data.data.current_condition[0].weatherDesc[0].value);
+				var w_desc = data.data.current_condition[0].weatherDesc[0].value;
+				$('#steamers .weatherDesc').text('Skies: ' + w_desc);
+				
+				},
+			error: function(e) {console.log('epic fail')}
+			});
+			
+		// Marine Conditions
+		
+		// Rincon Weather Conditions
+		var waves = $(this);
+		var api = "c9cda4e16df76d61eb092e6b5c5910ee3f0c6f3c";
+		var carpUrl = "http://api.worldweatheronline.com/free/v1/weather.ashx?q=93014&format=json&date=today&key=" + api;
+		$.ajax({
+			type: 'POST',
+			url: carpUrl,
+			dataType: 'jsonp',
+			data: waves.serialize(),
+			success: function(data){
+				
+				// local time 
+				console.log(data.data.current_condition[0].observation_time);
+
+				//temperature
+				console.log(data.data.current_condition[0].temp_F);
+				var temp = data.data.current_condition[0].temp_F;
+				$('#rincon .temp').text('Air Temp: ' + temp + ' degrees.');
+				
+				//wind direction
+				console.log(data.data.current_condition[0].winddirDegree);
+				var w_dir = data.data.current_condition[0].winddirDegree;
+				
+				if (w_dir < 0) {
+				var windDir = "North";
+					} else if (w_dir < 45) {
+				var windDir = "North East";		
+						} else if (w_dir < 90) {
+				var windDir = "East";			
+						} else if (w_dir < 135) {
+				var windDir = "South East";	
+							} else if (w_dir < 180) {
+				var windDir = "South";				
+								} else if (w_dir < 225) {
+				var windDir = "South West";		
+									} else if (w_dir < 270) {
+				var windDir = "West";	
+										} else if (w_dir < 315) {
+				var windDir = "North West";							
+											}
+				console.log(windDir);
+				
+				// wind speed
+				console.log(data.data.current_condition[0].windspeedMiles);
+				var w_speed = data.data.current_condition[0].windspeedMiles;
+				$('#rincon .windDir').text('Wind: From the ' + windDir + ' at ' + w_speed + ' mph');
+				
+				// weather Description
+				console.log(data.data.current_condition[0].weatherDesc[0].value);
+				var w_desc = data.data.current_condition[0].weatherDesc[0].value;
+				$('#rincon .weatherDesc').text('Skies: ' + w_desc);
+				
+				},
+			error: function(e) {console.log('epic fail')}
+			});
+			
+		// Trestles Weather Conditions
+		var waves = $(this);
+		var api = "c9cda4e16df76d61eb092e6b5c5910ee3f0c6f3c";
+		var trestlesUrl = "http://api.worldweatheronline.com/free/v1/weather.ashx?q=92674&format=json&date=today&key=" + api;
+		$.ajax({
+			type: 'POST',
+			url: trestlesUrl,
+			dataType: 'jsonp',
+			data: waves.serialize(),
+			success: function(data){
+				
+				// local time 
+				console.log(data.data.current_condition[0].observation_time);
+
+				//temperature
+				console.log(data.data.current_condition[0].temp_F);
+				var temp = data.data.current_condition[0].temp_F;
+				$('#trestles .temp').text('Air Temp: ' + temp + ' degrees.');
+				
+				//wind direction
+				console.log(data.data.current_condition[0].winddirDegree);
+				var w_dir = data.data.current_condition[0].winddirDegree;
+				
+				if (w_dir < 0) {
+				var windDir = "North";
+					} else if (w_dir < 45) {
+				var windDir = "North East";		
+						} else if (w_dir < 90) {
+				var windDir = "East";			
+						} else if (w_dir < 135) {
+				var windDir = "South East";	
+							} else if (w_dir < 180) {
+				var windDir = "South";				
+								} else if (w_dir < 225) {
+				var windDir = "South West";		
+									} else if (w_dir < 270) {
+				var windDir = "West";	
+										} else if (w_dir < 315) {
+				var windDir = "North West";							
+											}
+				console.log(windDir);
+				
+				// wind speed
+				console.log(data.data.current_condition[0].windspeedMiles);
+				var w_speed = data.data.current_condition[0].windspeedMiles;
+				$('#trestles .windDir').text('Wind: From the ' + windDir + ' at ' + w_speed + ' mph');
+				
+				// weather Description
+				console.log(data.data.current_condition[0].weatherDesc[0].value);
+				var w_desc = data.data.current_condition[0].weatherDesc[0].value;
+				$('#trestles .weatherDesc').text('Skies: ' + w_desc);
+				
+				},
+			error: function(e) {console.log('epic fail')}
+			});
+		
+		
+		
 });
