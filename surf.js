63666b000bd930e282b961c22e850138275d04d0
Brian Wynn <brian.wynn83@gmail.com>
2015-01-15 00:32:28 -0800

    incorporating different functions into the ajax call. left myself and unfinished fix.

--- surf.js
+++ surf.js
@@ -1,21 +1,33 @@
 var api = "c9cda4e16df76d61eb092e6b5c5910ee3f0c6f3c";
 var waves = $(this);
 
-$.ajax({
-  type: 'POST',
-  url: "http://api.worldweatheronline.com/free/v1/marine.ashx?q=36.5%2C-122&format=json&date=today&key=" + api,
-  dataType: 'jsonp',
-  data: waves.serialize(),
-  success: function(data){
-    var swellDir = data.data.weather[0].hourly[0].swellDir;         // gets swell direction
-    console.log(swellDir);                                          // prints to console
-    swDir();                                                        // calls swDir function
-  }
-});
+function santaCruzMarineCall() {
+  $.ajax({
+    type: 'POST',
+    url: "http://api.worldweatheronline.com/free/v1/marine.ashx?q=36.5%2C-122&format=json&date=today&key=" + api,
+    dataType: 'jsonp',
+    data: waves.serialize(),
+    success: function(data){
+      var swellDir = data.data.weather[0].hourly[0].swellDir;         // gets swell direction
+      var waterTemp = data.data.weather[0].hourly[0].waterTemp_F;     // gets water temp
+      var wSizeM = data.data.weather[0].hourly[0].swellHeight_m;      // gets swell height in meters
+      var wSizeF = (wSizeM * 3.28).toPrecision(3);                    // meters to feet
+      var sPeriod = data.data.weather[0].hourly[0].swellPeriod_secs;
 
-function swDir(data) {
-  var swellDir;
-  var sDir;
+      console.log('water temp today: ' + waterTemp);
+      console.log(swellDir);
+      console.log('wave height feet: ' + wSizeF);
+      console.log('swell period: ' + sPeriod + ' seconds');
+
+      swDir();                                                        // calls swDir function
+      wetsuit();                                                      // calls wetsuit function
+      swellPeriod();                                                  // calls swell period
+    }
+  });
+};
+
+function swDir() {
+  var swellDir = data.data.weather[0].hourly[0].swellDir;
   if ( swellDir < 23 ) {
      sDir = "NNE";
   } else if ( swellDir < 45 ) {
@@ -51,3 +63,39 @@ function swDir(data) {
   }
   console.log('Primary swell direction: ' + sDir + ' at ' + swellDir + ' degrees.')
 };
+
+// Wetsuit Recommendation
+function wetsuit() {
+  var waterTemp;
+  if ( waterTemp < 55 ) {
+    var wSuit = "5/4 Hooded Fullsuit";
+  } else if ( waterTemp < 60 ) {
+     wSuit = "4/3 Fullsuit";
+  } else if ( waterTemp < 67 ) {
+     wSuit = "3/2 Fullsuit";
+  } else if ( waterTemp < 72 ) {
+     wSuit = "Springsuit";
+  } else if ( waterTemp < 75 ) {
+     wSuit = "Vest & Trunks";
+  } else if ( waterTemp > 75 ) {
+     wSuit = "Trunks";
+  }
+  console.log('wetsuit: ' + wSuit);
+};
+
+// Swell Period
+function swellPeriod() {
+  var sPeriod;
+  if ( sPeriod < 7 ) {
+    var swellSig = "Junky, short-period windswell";
+  } else if ( sPeriod < 10 ) {
+    var swellSig = "Windswell";
+  } else if ( sPeriod < 12 ) {
+    var swellSig = "Short period ground swell";
+  } else if ( sPeriod > 12 ) {
+    var swellSig = "Long period ground swell";
+  }
+  console.log("Today's swell conditions: " + swellSig);
+};
+
+santaCruzMarineCall();
